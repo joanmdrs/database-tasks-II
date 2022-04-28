57048e52898fe3f329f6be69aefe59c3bbcc3236
Joan Medeiros <77677763+joanmdrs@users.noreply.github.com>
2022-04-28 14:45:14 -0300

    Criando o arquivo tarefa01-inserts.sql #1

---
+++ tarefas/t01/tarefa01-inserts.sql
@@ -0,0 +1,6 @@
+INSERT INTO funcionario (nome, sexo, dt_nasc, salario, cod_dep)
+VALUES("Joan de Azevedo Medeiros", 'M', '2000-12-11', 5000.00, NULL),
+("Allan Gabriel Almeida Oliveira", 'M', '2000-12-31', 5500.00, NULL),
+("Rauan Meirelles Dantas de Araujo", 'M', '2001-05-28', 1200.00, NULL),
+("Deyse de Azevedo Paiva", 'F', '2000-05-09', 3000.00, NULL),
+("Thaynara de Azevedo Luciano", 'F', '2000-12-03', 10000.00, NULL)
