1f814c47fde5f28c26550b957d52af22666c238d
Joan Medeiros <77677763+joanmdrs@users.noreply.github.com>
2022-05-10 17:56:39 -0300

    corrigindo a questão 10 #1

--- tarefas/t01/tarefa01-q10.sql
+++ tarefas/t01/tarefa01-q10.sql
@@ -1,4 +1,13 @@
 # Questão 10
-SELECT p.nome, d.descricao, f.nome
-FROM funcionario as f , departamento as d , projeto as p
-WHERE p.cod_depto = d.codigo and p.cod_responsavel = f.codigo 
+SELECT 
+	  p.nome as "projeto", 
+    d.descricao as "departamento", 
+    f.nome as "gerente",
+    df.descricao as "departamento gerente"
+FROM projeto as p
+JOIN departamento as d
+ON p.cod_depto = d.codigo
+JOIN funcionario as f
+ON p.cod_responsavel = f.codigo
+JOIN departamento as df
+ON f.cod_dep = df.codigo
