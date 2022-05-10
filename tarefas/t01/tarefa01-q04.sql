15bb68da150c16d98008a7b6a41791f70bf12868
Joan Medeiros <77677763+joanmdrs@users.noreply.github.com>
2022-05-10 15:28:02 -0300

    corrigindo a questão 04 #1

--- tarefas/t01/tarefa01-q04.sql
+++ tarefas/t01/tarefa01-q04.sql
@@ -1,4 +1,5 @@
 #Questão 04
-SELECT nome, salario, cod_dep
-from funcionario 
-WHERE codigo NOT IN (SELECT cod_gerente from departamento)
+SELECT f.nome, f.salario, f.cod_dep
+from funcionario as f
+WHERE f.codigo NOT IN (SELECT cod_gerente from departamento) 
+ORDER BY f.codigo
