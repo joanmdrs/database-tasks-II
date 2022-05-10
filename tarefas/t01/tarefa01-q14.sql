fda1e473913c4012c2c7fbca982e46dd1e1d58d4
Joan Medeiros <77677763+joanmdrs@users.noreply.github.com>
2022-05-10 18:01:49 -0300

    Corrigindo a questão 14 #1

--- tarefas/t01/tarefa01-q14.sql
+++ tarefas/t01/tarefa01-q14.sql
@@ -1,10 +1,10 @@
 #Criando a VIEW 
-CREATE VIEW vwAtividade (funcionario, quant) 
+CREATE VIEW vwInfoProjetos(funcionario, quant) 
 AS SELECT f.nome, count(a.codigo)
 from funcionario as f, atividade as a
 WHERE f.codigo = a.cod_responsavel
-GROUP by f.codigo
+GROUP by f.codigo;
 
 #Realizando a consulta 
 SELECT funcionario as "Funcionário responsável", quant as "Nº de atividades"
-from vwAtividade
+from vwInfoProjetos
