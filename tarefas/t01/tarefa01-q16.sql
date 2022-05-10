5cf38af064d21f825f4f88c0fe299d027a169f90
Joan Medeiros <77677763+joanmdrs@users.noreply.github.com>
2022-05-10 18:11:01 -0300

    corrigindo a questão 16 #1

--- tarefas/t01/tarefa01-q16.sql
+++ tarefas/t01/tarefa01-q16.sql
@@ -1,7 +1,16 @@
 # Questão 16
-SELECT f.nome
-from funcionario as f, vwResponsavel as vr
-WHERE f.codigo = vr.responsavel 
+
+#Criando a VIEW 
+CREATE VIEW vwResponsaveis(responsavel)
+AS SELECT f.codigo 
+from funcionario as f
+WHERE f.codigo IN (SELECT cod_responsavel from projeto)
+
+#Fazendo a consulta
+SELECT f.nome, f.salario
+from funcionario as f 
+JOIN vwResponsaveis as vr
+ON f.codigo = vr.responsavel 
 and f.codigo in (SELECt cod_gerente from departamento)
 or f.codigo = vr.responsavel 
 AND f.salario > (SELECT f.salario from funcionario as f, departamento as d 
