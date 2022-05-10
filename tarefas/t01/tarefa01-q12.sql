bb13d95e279356074e8b83f4f1e2425a228a1b7d
Joan Medeiros <77677763+joanmdrs@users.noreply.github.com>
2022-05-10 10:17:38 -0300

    Implementando a questão 12 #1

---
+++ tarefas/t01/tarefa01-q12.sql
@@ -0,0 +1,15 @@
+#Questão 10
+SELECT 
+	  p.nome, 
+    p.data_inicio, 
+    p.data_fim, 
+    a.descricao, 
+    a.data_inicio, 
+    a.data_fim
+from 
+    projeto as p, 
+    atividade as a, 
+    atividade_projeto as ap
+WHERE 
+    p.codigo = ap.cod_projeto 
+    and a.codigo = ap.cod_atividade
