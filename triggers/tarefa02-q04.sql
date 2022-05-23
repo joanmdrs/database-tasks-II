8eeca995c44b852010f81540c636a75753d551a5
Joan Medeiros <77677763+joanmdrs@users.noreply.github.com>
2022-05-23 14:56:16 -0300

    Atualizando a questão 02 #2

--- triggers/tarefa02-q04.sql
+++ triggers/tarefa02-q04.sql
@@ -1,7 +1,7 @@
 # 04. Crie um gatilho que ao se adicionar um novo cliente, 
 # seja criado para ele um cartão de milhagem com 0 milhas.
 
-CREATE TRIGGER addCartaoMilhas AFTER 
+CREATE TRIGGER addMileageCard AFTER 
 INSERT ON cliente 
 FOR EACH ROW 
 INSERT INTO milhas(cod_cliente, quantidade)
