645d209f8f940012aaa3f65fef0294ab51125249
Joan Medeiros <77677763+joanmdrs@users.noreply.github.com>
2022-05-23 14:58:36 -0300

    Update tarefa02-q07.sql

--- triggers/tarefa02-q07.sql
+++ triggers/tarefa02-q07.sql
@@ -2,7 +2,7 @@
 # a um voo sempre que um cliente entre em um voo. 
 
 # Criando o TRIGGER
-CREATE TRIGGER addPassageiroInTheVoo AFTER
+CREATE TRIGGER addPassengerOnFlight AFTER
 INSERT ON cliente_voo
 FOR EACH ROW
 UPDATE voo SET num_passageiros = num_passageiros + 1
@@ -14,7 +14,7 @@ VALUES("Allan Gabriel Almeida Oliveira", "Rua Ricardo Felix da Silva");
 
 # Inserindo um voo
 INSERT INTO voo(tipo, cod_piloto, num_passageiros, distancia)
-VALUES ("Executivo", 1, 10, 500.00);
+VALUES ("Executivo", 1, 0, 500.00);
 
 # Inserindo um passageiro no voo
 INSERT INTO cliente_voo(cod_cliente, cod_voo, classe)
