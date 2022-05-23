2d0cce48ffdef92c6a84214efd3682bd97c2058e
Joan Medeiros <77677763+joanmdrs@users.noreply.github.com>
2022-05-23 16:00:11 -0300

    Update tarefa02-q04.sql

--- tarefas/t02/tarefa02-q04.sql
+++ tarefas/t02/tarefa02-q04.sql
@@ -6,6 +6,3 @@ INSERT ON cliente
 FOR EACH ROW 
 INSERT INTO milhas(cod_cliente, quantidade)
 VALUES (NEW.codigo, 0); 
-
-INSERT INTO cliente(nome, endereco)
-VALUES ("Joan Medeiros", "Rua Ricardo Felix da Silva, Penedo, 554, apto 402, Caicó/RN");
