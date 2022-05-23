379fd72ebb868170a98612ac48825a8ab075404d
Joan Medeiros <77677763+joanmdrs@users.noreply.github.com>
2022-05-23 14:26:07 -0300

    Implementando a questão 07 #2

---
+++ triggers/tarefa02-q07.sql
@@ -0,0 +1,21 @@
+# 07.Crie um gatilho que adicione mais um passageiro 
+# a um voo sempre que um cliente entre em um voo. 
+
+# Criando o TRIGGER
+CREATE TRIGGER addPassageiroInTheVoo AFTER
+INSERT ON cliente_voo
+FOR EACH ROW
+UPDATE voo SET num_passageiros = num_passageiros + 1
+WHERE codigo = new.cod_voo;
+
+# Inserindo um cliente
+INSERT INTO cliente(nome, endereco)
+VALUES("Allan Gabriel Almeida Oliveira", "Rua Ricardo Felix da Silva");
+
+# Inserindo um voo
+INSERT INTO voo(tipo, cod_piloto, num_passageiros, distancia)
+VALUES ("Executivo", 1, 10, 500.00);
+
+# Inserindo um passageiro no voo
+INSERT INTO cliente_voo(cod_cliente, cod_voo, classe)
+VALUES (2, 1, "Executiva");
