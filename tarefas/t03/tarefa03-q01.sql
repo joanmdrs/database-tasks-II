194c31400c92215eb17abec31537084044eaa28d
Joan Medeiros <77677763+joanmdrs@users.noreply.github.com>
2022-05-24 17:21:56 -0300

    Implementando a questão 01 #3

---
+++ tarefas/t03/tarefa03-q01.sql
@@ -0,0 +1,7 @@
+# Questão 01
+
+/* Transação é um conceito fundamental de todo sistema de banco de dados. O ponto essencial da transação
+é englobar vários passos em uma única operação de tudo ou nada. Os estados intermediários entre os 
+passos não são vistos pelas demais transações simultâneas e, se ocorrer alguma falha que impeça a 
+transação chegar até o fim, então nenhum dos passos intermediários irá afetar o banco de dados de 
+forma alguma. */
