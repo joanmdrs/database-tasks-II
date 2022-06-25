ae906672484dd76c58481b5c8354ce4e1d4b048c
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-06-25 16:14:13 -0300

    Implementando o select de projetos e suas atividades #5

---
+++ tarefas/t03/connection_orm/select.py
@@ -0,0 +1,13 @@
+from models import *
+from connection import connectDatabase, closeConnection
+
+connectDatabase()
+
+def selectProjetoJoinAtividades():
+    return Projeto.select(Projeto.codigo, Projeto.descricao, Atividade.descricao).join(Atividade).where(Projeto.codigo == Atividade.codprojeto)
+
+
+query = selectProjetoJoinAtividades()
+
+for projeto in query:
+    print(projeto.codigo, "-", projeto.descricao,":", projeto.atividade.descricao)
