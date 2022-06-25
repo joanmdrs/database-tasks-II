7ec9b86a023f56180b3c92de5cddd190ec751411
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-06-25 16:37:56 -0300

    Implementando a função de update sem o uso do orm #5

---
+++ tarefas/t03/pure_connection/update.py
@@ -0,0 +1,20 @@
+from datetime import date
+from connect import connectDataBase
+    
+conn = connectDataBase()
+cur = conn.cursor()
+
+def updateResponsavelProjeto(codProjeto, novoResponsavel):
+    
+    update_script = '''
+        UPDATE projeto SET codresponsavel = %s
+        WHERE codigo = %s;
+    '''
+    update_value = (novoResponsavel, codProjeto)
+    cur.execute(update_script, update_value)
+    conn.commit()
+
+updateResponsavelProjeto(codProjeto=2, novoResponsavel=4)
+
+cur.close()
+conn.close()
