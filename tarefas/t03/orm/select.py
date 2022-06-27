879a60ee7f3033c8b1dda4397bb85f2101e181f4
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-06-27 10:52:46 -0300

    formatando a exibição da consulta #5

--- tarefas/t03/orm/select.py
+++ tarefas/t03/orm/select.py
@@ -4,10 +4,43 @@ from connect import connectDatabase, closeConnection
 connectDatabase()
 
 def selectProjetoJoinAtividades():
-    return Projeto.select(Projeto.codigo, Projeto.descricao, Atividade.descricao).join(Atividade).where(Projeto.codigo == Atividade.codprojeto)
+    return Projeto.select(
+        Projeto.codigo,
+        Projeto.nome, 
+        Projeto.descricao,
+        Projeto.codresponsavel,
+        Projeto.coddepto,
+        Projeto.datainicio, 
+        Projeto.datafim,
+        Atividade.codigo,
+        Atividade.descricao,
+        Atividade.codprojeto,
+        Atividade.datainicio, 
+        Atividade.datafim
+        ).join(Atividade).where(Projeto.codigo == Atividade.codprojeto)
 
 
 query = selectProjetoJoinAtividades()
 
+print("%-97s | %-20s" %('Projeto', 'Atividade'))
+print("%173s" %("-"*173))
 for projeto in query:
-    print(projeto.codigo, "-", projeto.descricao,":", projeto.atividade.descricao)
+    print("%-4d %-15s %-40s %4s %4s %12s %12s | %-4d %-35s %4s %12s %12s" % (
+        projeto.codigo,
+        projeto.nome, 
+        projeto.descricao,
+        str(projeto.codresponsavel),
+        str(projeto.coddepto),
+        projeto.datainicio.strftime("%d/%m/%Y"), 
+        projeto.datafim.strftime("%d/%m/%Y"),
+        projeto.atividade.codigo,
+        projeto.atividade.descricao,
+        str(projeto.atividade.codprojeto),
+        projeto.atividade.datainicio.strftime("%d/%m/%Y"), 
+        projeto.atividade.datafim.strftime("%d/%m/%Y"),
+    ))
+        
+        
+        
+        
+    
